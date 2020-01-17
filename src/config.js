8897104269dc7422384bc6c0c4cda6dfab3587e5
Mateus.Cassandri <mateus.cassandri@senior.com.br>
2020-01-17 11:08:44 -0300

    Configurando React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "buckets-uploads-notes"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://941gvvlcsg.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_i8Zh6MVjr",
+      APP_CLIENT_ID: "38uq4tvnu7mvkd6qjs4d45rvu0",
+      IDENTITY_POOL_ID: "us-east-1:7853f553-f1b3-49cf-bb43-7addfa8b4067"
+    }
+  };
